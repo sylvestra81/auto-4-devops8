f8ced376b38e9fea6e3feed2547ff168dd8d6143
root <root@ip-172-31-94-167.ec2.internal>
2023-08-02 10:43:56 +0000

    my first commit

---
+++ myapp/src/test/java/slycode/AppTest.java
@@ -0,0 +1,20 @@
+package slycode;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
